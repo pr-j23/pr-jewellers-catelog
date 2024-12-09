285081445d0b8d7f346dab4b01dfe276c4ab0823
pr-j23 <pavanraj577@gmail.com>
2024-12-09 20:33:41 +0530

    slice file name changed

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import productsReducer from './slices/productsSlice';
-import metalPricesReducer from './slices/metalpricesSlice';
+import metalPricesReducer from './slices/metalPricesSlice';
 import cartReducer from './slices/cartSlice';
 
 export const store = configureStore({
